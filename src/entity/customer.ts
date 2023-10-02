cc581c5ee9ebc882ddf10c0224dbf842e5eb0772
Artur Bruno <artur.bruno@intmed.com.br>
2023-10-02 15:49:43 -0300

    Costumer unit test coverage

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -2,10 +2,10 @@
 import Address from "./address";
 
 export default class Customer {
-    _id: string;
-    _name: string = "";
-    _address!: Address;
-    _active: boolean = true;
+    private _id: string;
+    private _name: string = "";
+    private _address!: Address;
+    private _active: boolean = true;
 
     constructor(id: string, name: string) {
         this._id = id;
@@ -13,10 +13,17 @@ export default class Customer {
         this.validate();
     }
 
-    //auto-validade
+    get name(): string {
+        return this._name;
+    }
+
+    get isActive(): boolean {
+        return this._active;
+    }
+
     validate() {
-        if (this._name.length === 0) throw new Error("Name is required");
         if (this._id.length === 0) throw new Error("Id is required");
+        if (this._name.length === 0) throw new Error("Name is required");
     }
 
     changeName(name: string) {
@@ -26,7 +33,7 @@ export default class Customer {
 
     activate() {
         if (this._address === undefined) {
-            throw new Error("Address is mandatory to activa a customer");
+            throw new Error("Address is mandatory to activate a customer");
         }
         this._active = true;
     }
