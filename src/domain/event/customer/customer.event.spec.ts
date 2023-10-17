f13d54f3823819cbc0052f0b94cc534ede3c194e
Artur Bruno <artur.bruno@intmed.com.br>
2023-10-17 12:53:46 -0300

    Adding customer domain events and covering with unit tests

---
+++ src/domain/event/customer/customer.event.spec.ts
@@ -0,0 +1,58 @@
+import EventDispatcher from "../@shared/event-dispatcher";
+import EnviaConsoleLog1Handler from "./handler/envia-console-log1-handler";
+import EnviaConsoleLog2Handler from "./handler/envia-console-log2-handler";
+import CustomerCreatedEvent from "./customer-created.event";
+import Customer from "../../entity/customer";
+import Address from "../../entity/address";
+import EnviaConsoleLogHandler from "./handler/envia-console-log-handler";
+import CustomerUpdatedEvent from "./customer-updated.event";
+
+describe('Customer Events Test', () => {
+
+    it('should notify handlers when a customer is created', async () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler1 = new EnviaConsoleLog1Handler();
+        const eventHandler2 = new EnviaConsoleLog2Handler();
+        const spyEventHandler1 = jest.spyOn(eventHandler1, "handle");
+        const spyEventHandler2 = jest.spyOn(eventHandler2, "handle");
+
+        eventDispatcher.register("CustomerCreatedEvent", eventHandler1);
+        eventDispatcher.register("CustomerCreatedEvent", eventHandler2);
+
+        expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]).toMatchObject(eventHandler1);
+        expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][1]).toMatchObject(eventHandler2);
+
+        const customer = new Customer("1", "Customer 1");
+        customer.Address = new Address("Street 1", 1, "Zipcode 1", "City 1");
+
+        const customerCreatedEvent = new CustomerCreatedEvent(customer);
+
+        eventDispatcher.notify(customerCreatedEvent);
+
+        expect(spyEventHandler1).toHaveBeenCalled();
+        expect(spyEventHandler2).toHaveBeenCalled();
+    });
+
+    it('should notify handlers when a customer is updated', async () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new EnviaConsoleLogHandler();
+        const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+        eventDispatcher.register("CustomerUpdatedEvent", eventHandler);
+
+        expect(eventDispatcher.getEventHandlers["CustomerUpdatedEvent"][0]).toMatchObject(eventHandler);
+
+        const customer = new Customer("1", "Customer 1");
+        customer.Address = new Address("Street1", 1, "Zipcode1", "City1");
+
+        const newAddress = new Address("Street2", 2, "Zipcode2", "City2");
+        customer.changeAddress(newAddress);
+
+        const customerUpdatedEvent = new CustomerUpdatedEvent(customer);
+
+        eventDispatcher.notify(customerUpdatedEvent);
+
+        expect(spyEventHandler).toHaveBeenCalled();
+    });
+
+});
---
+++ src/domain/event/customer/handler/envia-console-log-handler.ts
@@ -0,0 +1,10 @@
+import EventHandlerInterface from "../../@shared/event-handler.interface";
+import CustomerCreatedEvent from "../customer-created.event";
+import CustomerUpdatedEvent from "../customer-updated.event";
+
+export default class EnviaConsoleLogHandler implements EventHandlerInterface<CustomerUpdatedEvent> {
+    handle(event: CustomerUpdatedEvent): void {
+        const { id, name, Address } = event.eventData;
+        console.log(`Endereço do cliente: ${id}, ${name} alterado para: ${Address.street}, ${Address.number}, ${Address.city}, ${Address.zip}`);
+    }
+}
